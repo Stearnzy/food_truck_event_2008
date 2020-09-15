39d94470594de2156ec89e320f30d93c829084ca
Zach Stearns <zach.stearns92@gmail.com>
2020-09-15 13:04:10 -0600

    Add existence test

--- test/foodtruck_test.rb
+++ test/foodtruck_test.rb
@@ -3,5 +3,8 @@ require "minitest/pride"
 require "./lib/foodtruck"
 
 class FoodTruckTest < Minitest::Test
-
+  def test_it_exists
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    assert_instance_of FoodTruck, food_truck
+  end
 end
