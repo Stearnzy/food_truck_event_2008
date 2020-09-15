0e19e57cf49dba1b2d3c98d5b637ec7dd051b924
Zach Stearns <zach.stearns92@gmail.com>
2020-09-15 13:21:12 -0600

    Add check stock test

--- test/foodtruck_test.rb
+++ test/foodtruck_test.rb
@@ -1,10 +1,13 @@
 require "minitest/autorun"
 require "minitest/pride"
+require './lib/item'
 require "./lib/foodtruck"
 
 class FoodTruckTest < Minitest::Test
   def test_it_exists
     food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
     assert_instance_of FoodTruck, food_truck
   end
 
@@ -12,5 +15,17 @@ class FoodTruckTest < Minitest::Test
     food_truck = FoodTruck.new("Rocky Mountain Pies")
     assert_equal "Rocky Mountain Pies", food_truck.name
     assert_equal ({}), food_truck.inventory
+  end
+
+  def test_check_stock
+    food_truck = FoodTruck.new("Rocky Mountain Pies")
+    item1 = Item.new({name: 'Peach Pie (Slice)', price: "$3.75"})
+    item2 = Item.new({name: 'Apple Pie (Slice)', price: '$2.50'})
+
+    assert_equal 0, food_truck.check_stock(item1)
+
+    food_truck.stock(item1, 30)
+    
+    assert_equal ({item1 => 30}), food_truck.inventory
   end
 end
