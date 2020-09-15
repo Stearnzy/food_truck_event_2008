0c3cd2a4beb46fd1a0ee3c5bb8282d9ab10720e3
Zach Stearns <zach.stearns92@gmail.com>
2020-09-15 13:34:34 -0600

    Add existence and read name tests

--- test/event_test.rb
+++ test/event_test.rb
@@ -5,5 +5,13 @@ require "./lib/foodtruck"
 require "./lib/event"
 
 class EventTest < Minitest::Test
+  def test_it_exists
+    event = Event.new("South Pearl Street Farmers Market")
+    assert_instance_of Event, event
+  end
 
+  def test_readable_name
+    event = Event.new("South Pearl Street Farmers Market")
+    assert_equal "South Pearl Street Farmers Market", event.name
+  end
 end
