6dc7632d8a2d28d88719984cd3d82de7ef37eae3
Zach Stearns <zach.stearns92@gmail.com>
2020-09-15 13:20:58 -0600

    Add check_stock and stock methods

--- lib/foodtruck.rb
+++ lib/foodtruck.rb
@@ -1,8 +1,22 @@
 class FoodTruck
-  attr_reader :name
+  attr_reader :name, :inventory
 
   def initialize(name)
     @name = name
     @inventory = {}
+  end
+
+  def check_stock(item)
+    if @inventory[item].nil?
+      0
+    else
+      @inventory[item]
+    end
+  end
+
+# May need to alter to add count to existing
+#     stock
+  def stock(item, item_count)
+    @inventory[item] = item_count
   end
 end
