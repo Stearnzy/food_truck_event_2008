e80827549db142663c19d2c97eb08a5e4e1d6f92
Zach Stearns <zach.stearns92@gmail.com>
2020-09-15 14:40:00 -0600

    add total inventory method

--- lib/event.rb
+++ lib/event.rb
@@ -21,4 +21,35 @@ class Event
       truck.inventory.keys.include?(item)
     end
   end
+
+  def all_items
+    items = @food_trucks.map do |truck|
+      truck.inventory.keys
+    end.flatten
+    items.uniq
+  end
+
+  def blank_event_inventory
+    event_inventory = Hash.new(0)
+    all_items.each do |item|
+      event_inventory[item] = {}
+    end
+    event_inventory
+  end
+
+  def inventories_record(item)
+    count = 0
+    @food_trucks.each do |truck|
+      count += truck.check_stock(item)
+    end
+    inventories = {quantity: count, food_trucks: food_trucks_that_sell(item)}
+  end
+
+  def total_inventory
+    complete = {}
+    all_items.each do |item|
+      complete[item] = inventories_record(item)
+    end
+    complete
+  end
 end
